49ad5bd66306da4435be123f47643a898af1bb92
Dominic H. Liebel <fumpyy@gmail.com>
2016-07-08 10:58:26 +0200

    Learning with Bucky

--- Bucky/Bucky.cpp
+++ Bucky/Bucky.cpp
@@ -1,11 +1,16 @@
-// Bucky.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
+#include <iostream>
+using namespace std;
+int addNumbers(int x, int y);
 
 
-int main()
-{
+
+int main() {
+    cout << addNumbers(43, 5) << "\n";
     return 0;
 }
 
+int addNumbers(int x, int y){
+  int answer = x + y;
+  return answer;
+}
